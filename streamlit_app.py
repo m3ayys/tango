03213f34dd264d4543de1539ea11e82bb91278a8
m3ayys <144980844+m3ayys@users.noreply.github.com>
2023-09-22 00:08:25 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,45 +2,31 @@
 import streamlit as st
 import random
 
-# Define the quotes and their vocabulary words
-quotes = {
-    "Abraham Lincoln": {
-        "quote": "Nearly all men can stand adversity, but if you want to test a man's character, give him power.",
-        "vocab": ["adversity", "character"]
-    },
-    "Martin Luther King Jr.": {
-        "quote": "The time is always right to do what is right.",
-        "vocab": ["time", "right"]
-    },
-    "Eleanor Roosevelt": {
-        "quote": "The future belongs to those who believe in the beauty of their dreams.",
-        "vocab": ["future", "beauty", "dreams"]
-    }
-}
-
-# Define the quiz function
-def quiz():
-    # Select a random quote
-    author, quote = random.choice(list(quotes.items()))
-    # Select a random vocabulary word
-    vocab_word = random.choice(quote["vocab"])
-    # Ask the user to provide the definition
-    definition = st.text_input(f"What is the definition of '{vocab_word}' in this quote by {author}?\n\n{quote['quote']}")
-    # Check the answer
-    if definition.lower() == vocab_word.lower():
-        st.write("Correct!")
-    else:
-        st.write(f"Sorry, the correct answer was '{vocab_word}'.")
+# Define the quotes
+quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "The best way to predict your future is to create it. - Abraham Lincoln",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill",
+    "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+    "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle. - Christian D. Larson",
+    "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
+    "If you can dream it, you can achieve it. - Zig Ziglar",
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt"
+]
 
 # Define the sidebar
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Quiz"])
+page = st.sidebar.radio("Go to", ["Home", "Fortune"])
 
 # Define the pages
 if page == "Home":
     st.title("Famous American Quotes")
-    for author, quote in quotes.items():
-        st.write(f"**{author}:** {quote['quote']}")
-elif page == "Quiz":
-    st.title("Vocabulary Quiz")
-    quiz()
+    st.write("Welcome to the Famous American Quotes app! Click on 'Fortune' in the sidebar to get your daily quote.")
+elif page == "Fortune":
+    st.title("Your Daily Fortune")
+    st.write("Click the button below to get your daily quote.")
+    if st.button("Get Quote"):
+        quote = random.choice(quotes)
+        st.write(quote)
